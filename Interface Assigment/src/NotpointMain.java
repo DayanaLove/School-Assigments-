989ef84c92b24c376b93ae706c390326f26058dd
Dayana Love <74844257+DayanaLove@users.noreply.github.com>
2020-11-23 09:50:51 -0500

    Interface Assigment

---
+++ Interface Assigment/src/NotpointMain.java
@@ -0,0 +1,21 @@
+
+public class NotpointMain {
+
+	
+	public static void main(String[] args) throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		Point p1 = new Point(3, 4);
+		Point p2 = new Point(3.4, 1.4); 
+		Point p3=p2;
+		
+		System.out.println("p1.equals(p2):" + p1.equals(p2));
+		System.out.println("p2.equals(p3):"+ p2.equals(p3));
+		System.out.println("p1.compareTo(p2):" + p1.compareTo(p2));
+		System.out.println("p2.compareTo(p3):" + p2.compareTo(p3));
+		Point p4= (Point)(p1.clone());
+		System.out.println("p4 is a clone of p1 so it is equal: " + p4.equals(p1));
+		System.out.println("p4 is a clone of p1, p4.compareTo(p1): " + p4.compareTo(p1));
+	
+	}
+
+}
---
+++ Interface Assigment/src/Point.java
@@ -0,0 +1,79 @@
+
+public class Point implements Cloneable, Comparable<Point> {
+	
+ double x=0;
+ double y=0;
+
+
+ public double getX() {
+	 return this.x;
+ } 
+ 
+ public double getY() {
+	 return this.y;
+ }
+ 
+ Point(){}
+ 
+ Point(double x, double y){
+	 this.x=x;
+	 this.y=y;
+}
+ 
+
+public int compareTo(Point o) {
+	
+	if (computeL() > o.computeL())
+return 1;
+
+else if (computeL() < o.computeL())
+
+return -1;
+
+else;
+
+return 0;	
+
+}
+
+public double computeL() {
+	 double xsquared=Math.pow(x, 2); 
+	 double ysquared=Math.pow(y, 2); 
+	 double  L= Math.sqrt(xsquared+ysquared);
+	return L;
+}
+
+@Override
+public boolean equals(Object o){
+	Point p = (Point) o;
+	
+	 if (getX() == p.getX() && getY() == p.getY())
+		 return true;
+	 else 
+		 return false;
+		 
+}
+
+@Override
+public String toString(){
+	return "[" + x + y + "]"; 
+}
+
+@Override
+public Object clone() throws CloneNotSupportedException  {
+	Point pointClone = (Point)super.clone();
+	return pointClone;
+	}
+
+}
+
+
+
+
+
+
+
+
+	 
+ 
+ 
