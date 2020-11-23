52551ca1e6946fdfe52c954662508952854f910d
Dayana Love <74844257+DayanaLove@users.noreply.github.com>
2020-11-23 09:52:55 -0500

    TextIO Assignment

---
+++ TextIO Project/src/dayanaLove_TextIO/Index.java
@@ -0,0 +1,101 @@
+package dayanaLove_TextIO;
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Index {
+
+String inputFileName;
+ArrayList <String> list = new ArrayList<String>();
+
+
+
+Index(String inputFileName){
+	this.inputFileName = inputFileName;
+}
+	
+	
+
+public ArrayList<String> listArray() throws FileNotFoundException {
+	
+// converts text file to list array	
+    File file = new File(inputFileName);
+    Scanner scan = new Scanner(file);
+
+    while(scan.hasNextLine()) {
+            list.add(scan.nextLine());
+    }
+
+    ListIterator<String> iterator = list.listIterator();
+// makes list lower case 
+while (iterator.hasNext())
+{
+    iterator.set(iterator.next().toLowerCase());
+}
+// sorts by alphabetical order 
+scan.close();
+Collections.sort(list);
+
+// removes duplicates 
+LinkedHashSet<String> hashSet = new LinkedHashSet<>(list);
+ArrayList<String> duplicateFreeList = new ArrayList<>(hashSet);
+
+
+return duplicateFreeList;
+}
+
+
+
+
+
+public void getIndexLength(){
+   System.out.print(list.size());
+}
+	
+public boolean addWord(String newWord){
+	
+  if (list.contains(newWord)){
+	  return false;
+  } else if (!list.contains(newWord))
+	list.add(newWord);
+    return true;
+		 	
+}
+
+
+public boolean containsWord(String word){
+	
+	  if (list.contains(word)){
+		  return true;
+	  } 
+	    return false;		 	
+	}
+
+
+
+public void writeIndexToFile(String outputFileName) throws IOException {
+	
+	FileWriter writer = new FileWriter(outputFileName); 
+	for(String str: list) {
+	  writer.write(str + System.lineSeparator());
+	}
+	
+	writer.close();
+	
+}
+
+
+
+
+}
---
+++ TextIO Project/src/dayanaLove_TextIO/Test.java
@@ -0,0 +1,43 @@
+package dayanaLove_TextIO;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		
+	
+		Index index = new Index("/Users/Featherberry/Desktop/TextIOInput.txt");
+		System.out.println(index.listArray());
+		index.addWord("Apple");
+		System.out.println(index.listArray());
+	    index.writeIndexToFile("test3");
+        
+		
+		
+	}
+
+	
+			
+			
+		
+	
+
+
+
+}
+	
+	
+
